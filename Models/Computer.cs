cf3e191e1fbc324019c9652309028d8d0d1f0de0
yuryi.ustinovich <ustinhtc@gmail.com>
2021-09-04 18:25:59 +0300

    Add project files.

---
+++ Models/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.SqlServer.Server;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoProject1.Models
+{
+    public class Computer
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name []
+    }
+}
